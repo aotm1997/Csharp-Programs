222e868c9cc9e7e8a9f9cbd7c17a19ba9db39a84
aotm1997 <53805991+aotm1997@users.noreply.github.com>
2022-04-17 21:33:05 +0700

    Update and rename 1 to 1.cs

---
+++ Algebra_7_class/Никольский/1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Натуральными числами называются те числа, которые мы используем при счете. " +
+                "Ноль не является натуральным числом");
+        }
+    }
+}
