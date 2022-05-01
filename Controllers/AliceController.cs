633d9c73e6619b1644f36c5a3b7310d257ed93e1
aotm <roma.neugodnikov@yandex.ru>
2022-05-01 23:46:21 +0700

    serialization2

--- Controllers/AliceController.cs
+++ Controllers/AliceController.cs
@@ -4,25 +4,28 @@ using AliceHook.Models;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+
 namespace AliceHook.Controllers
 {
     [ApiController]
     [Route(template: "/")]
-    private static readonly JsonSerializerSettings ConverterSettings = new JsonSerializerSettings
-    {
-        ContractResolver = new DefaultContractResolver
-        {
-            NamingStrategy = new SnakeCaseNamingStrategy()
-        }
-    }
     public class AliceController : ControllerBase
-    {   
+    {
+     private static readonly JsonSerializerSettings ConverterSettings = new JsonSerializerSettings
+     {
+         ContractResolver = new DefaultContractResolver
+         {
+             NamingStrategy = new SnakeCaseNamingStrategy()
+         }
+     }
+    }
+       
         [HttpGet]
         public string Get()
         {
             return "It works!";
         }
-    }
+    
         [HttpPost]
         public Task Post()
         {
