141e4f92e544d4f61c285665f1f9e7a4f9303ef4
aotm <roma.neugodnikov@yandex.ru>
2022-05-02 00:46:18 +0700

    initialize3

--- Controllers/AliceController.cs
+++ Controllers/AliceController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.IO;
-using AliceHook.Models;
-using Microsoft.AspNetCore.Http;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -11,32 +9,18 @@ namespace AliceHook.Controllers
     [Route(template: "/")]
     public class AliceController : ControllerBase
     {
-     private static readonly JsonSerializerSettings ConverterSettings = new JsonSerializerSettings
-     {
-         ContractResolver = new DefaultContractResolver
-         {
-             NamingStrategy = new SnakeCaseNamingStrategy()
-         }
-     }
-    }
-       
+        private static readonly JsonSerializerSettings ConverterSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            }
+        };
+        
         [HttpGet]
         public string Get()
         {
             return "It works!";
         }
-    
-        [HttpPost]
-        public Task Post()
-        {
-            using var reader = new StreamReader(Request.Body);
-            var body = reader.ReadToEnd();
-            var aliceRequest = Json.Convert.DeserializeObject<AliceRequest>(body, ConverterSettings);
-            var aliceResponse = new AliceResponse(aliceRequest)
-            {
-                Response = Text = "Привет";
-            }
-            var stringResponse = JsonConvert.SerializeObject(aliceResponse, ConverterSettings);
-            return Response.WriteAsync(stringResponse);
-        }
+    }
 }
--- Models/AliceResponse.cs
+++ Models/AliceResponse.cs
@@ -1,15 +1,6 @@
 using System.Collections.Generic;
 namespace AliceHook.Models
 {
-public class Card
-    {
-        public string Type { get; set; }
-    }
-
-    public class Payload
-    {
-    }
-
     public class Button
     {
         public string Title { get; set; }
@@ -17,65 +8,17 @@ public class Card
         public string Url { get; set; }
         public bool Hide { get; set; }
     }
-
-    public class Directives
-    {
-    }
-
     public class Response
     {
         public string Text { get; set; }
         public string Tts { get; set; }
-        public Card Card { get; set; }
-        public IList<Button> Buttons { get; set; }
+        public List<Button> Buttons { get; set; }
         public bool EndSession { get; set; }
-        public Directives Directives { get; set; }
     }
-
-    public class SessionState
-    {
-        public int Value { get; set; }
-    }
-
-    public class UserStateUpdate
-    {
-        public int Value { get; set; }
-    }
-
-    public class ApplicationState
-    {
-        public int Value { get; set; }
-    }
-
-    public class SecondField
-    {
-        public string ThirdField { get; set; }
-    }
-
-    public class Value
-    {
-        public string Field { get; set; }
-        public SecondField SecondField { get; set; }
-    }
-
-    public class Event
-    {
-        public string Name { get; set; }
-        public Value Value { get; set; }
-    }
-
-    public class Analytics
-    {
-        public IList<Event> Events { get; set; }
-    }
-
     public class AliceResponse
     {
         public Response Response { get; set; } = new Response();
-        public SessionState SessionState { get; set; }
-        public UserStateUpdate UserStateUpdate { get; set; }
-        public ApplicationState ApplicationState { get; set; }
-        public Analytics Analytics { get; set; }
+        public Session Session { get; set; }
         public string Version { get; set; }
         public AliceResponse(AliceRequest request)
         {
